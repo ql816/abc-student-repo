f1f919ce905d0006bfedf890b9811ad15e19e3b9
Carol <ql816@nyu.edu>
2020-09-16 16:00:08 +0800

    edited js file

--- projects/coding-foundation/lib1.js
+++ projects/coding-foundation/lib1.js
@@ -6,14 +6,14 @@ function ClickButton()
 {
 
   var increment = document.getElementById("number").value;
-  increment = parseInt(increment)
+  increment = parseInt(increment);
   number_of_stars += increment;
   var br = document.createElement("br");
   for (i=0;i<increment;i++){
     var image = new Image();
     image.src = 'star.png';
-    image.style.width = '3%'
-    image.style.height = 'auto'
+    image.style.width = '3%';
+    image.style.height = 'auto';
     document.body.appendChild(image);
 
 
