cf5062629f2844e19cd8654879b37d771ad7a48f
Carol <ql816@nyu.edu>
2020-09-16 14:20:52 +0800

    coding-foundation exercise ✌️

---
+++ projects/coding-foundation/lib1.js
@@ -0,0 +1,23 @@
+
+var number_of_stars = 0;
+
+
+function ClickButton()
+{
+
+  var increment = document.getElementById("number").value;
+  increment = parseInt(increment)
+  number_of_stars += increment;
+  var br = document.createElement("br");
+  for (i=0;i<increment;i++){
+    var image = new Image();
+    image.src = 'star.png';
+    image.style.width = '3%'
+    image.style.height = 'auto'
+    document.body.appendChild(image);
+
+
+  }
+
+
+}
