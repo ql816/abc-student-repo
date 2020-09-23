9d43399386706f5a0a3ed574b4518f5e0d6af60b
Carol <ql816@nyu.edu>
2020-09-23 21:13:22 +0800

    updated miniproject1 for gallery

---
+++ Carol_room/script.js
@@ -0,0 +1,7 @@
+
+var mini_1 = document.getElementById('promini1');
+
+mini_1.addEventListener("click",function(){
+
+  window.open("https://ql816.github.io/abc-student-repo/projects/miniproject1/");
+});
