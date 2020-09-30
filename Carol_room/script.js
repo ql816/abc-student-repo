5dead3869901280431ec1f1fdcf3fe5807807d5f
Carol <ql816@nyu.edu>
2020-09-30 21:07:18 +0800

    updated homepage

--- Carol_room/script.js
+++ Carol_room/script.js
@@ -1,7 +1,15 @@
 
 var mini_1 = document.getElementById('promini1');
+var mini_2 = document.getElementById('promini2');
+
 
 mini_1.addEventListener("click",function(){
 
   window.open("https://ql816.github.io/abc-student-repo/projects/miniproject1/");
+});
+
+
+mini_2.addEventListener("click",function(){
+
+  window.open("https://ql816.github.io/abc-student-repo/projects/miniproject2/");
 });
