25b8495ab97d3eea87edc91eebc9eaeb2eeff6f9
Carol <ql816@nyu.edu>
2020-10-07 20:34:09 +0800

    updated homepage

--- Carol_room/script.js
+++ Carol_room/script.js
@@ -13,3 +13,8 @@ mini_2.addEventListener("click",function(){
 
   window.open("https://ql816.github.io/abc-student-repo/projects/miniproject2/");
 });
+
+mini_3.addEventListener("click",function(){
+
+  window.open("https://ql816.github.io/abc-student-repo/projects/miniproject3/");
+});
