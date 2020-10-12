2643dbbdf69e80351138ea066f1252ec54320c71
Carol <ql816@nyu.edu>
2020-10-12 17:26:49 +0800

    updated homepage

--- Carol_room/script.js
+++ Carol_room/script.js
@@ -2,7 +2,7 @@
 var mini_1 = document.getElementById('promini1');
 var mini_2 = document.getElementById('promini2');
 var mini_3 = document.getElementById('promini3');
-
+var proja = document.getElementById('proA');
 mini_1.addEventListener("click",function(){
 
   window.open("https://ql816.github.io/abc-student-repo/projects/miniproject1/");
@@ -17,4 +17,10 @@ mini_2.addEventListener("click",function(){
 mini_3.addEventListener("click",function(){
 
   window.open("https://ql816.github.io/abc-student-repo/projects/miniproject3/");
+});
+
+
+proja.addEventListener("click",function(){
+
+  window.open("https://ql816.github.io/abc-student-repo/projects/projectA/");
 });
